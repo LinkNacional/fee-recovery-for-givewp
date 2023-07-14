3e2712cf043b2c64a76f53b4b9b9b736aa78e9e3
emanuellopess <lopes.emanuel@linknacional.com>
2023-07-14 19:40:46 +0000

    fix: Description fee is now updating correctly on runtime

--- public/js/fee-recovery-for-givewp-public.js
+++ public/js/fee-recovery-for-givewp-public.js
@@ -2,10 +2,6 @@
 	'use strict';
 
 	$(window).on('load', () => {
-		/* 		var feeGateways = Object.entries(JSON.parse($('#lkn-fee-recovery-fee-gateway').val()));
-				var giveGateways = document.getElementsByClassName('give-gateway')[1].getAttribute('checked');
-				console.log('recovery fee gateway: ' + JSON.stringify(feeGateways) + ' give gateway: ' + giveGateways);
-		 */
 		var iframe = document.getElementsByName('give-embed-form')[0];
 
 		if (iframe) {
@@ -20,6 +16,20 @@
 			styleLink.setAttribute('type', 'text/css');
 			var bodyIframe = iframe.contentDocument.getElementsByClassName('give-form')[0];
 			bodyIframe.appendChild(styleLink);
+
+			var inputAmount = iframe.contentDocument.getElementById('give-amount');
+			inputAmount.addEventListener('change', (event) => {
+				var amount = parseFloat(event.target.value);
+				update_fee(amount);
+			});
+
+			var donationLevelWrap = iframe.contentDocument.getElementById('give-donation-level-button-wrap');
+			donationLevelWrap.addEventListener('click', (event) => {
+				setTimeout(() => {
+					var amount = parseFloat(iframe.contentDocument.getElementById('give-amount').value);
+					update_fee(amount);
+				}, 500);
+			});
 
 			// Compatibility to add events for iframe forms
 			var inputVal = iframe.contentDocument.getElementById('lkn-fee-recovery-input');
@@ -62,52 +72,56 @@
 	}
 
 	function get_selected_gateway(iframe) {
+		var giveGateways = [];
+
 		if (iframe) {
-			var giveGateways = iframe.contentDocument.getElementsByClassName('give-gateway');
-
-			giveGateways.forEach(gateway => {
-				var isChecked = gateway.getAttribute('checked');
-				if (isChecked) {
-					return gateway
-				}
-			});
+			giveGateways = iframe.contentDocument.getElementsByClassName('give-gateway');
 		} else {
-			var giveGateways = document.getElementsByClassName('give-gateway');
-
-			giveGateways.forEach(gateway => {
-				var isChecked = gateway.getAttribute('checked');
-				if (isChecked) {
-					return gateway
-				}
-			});
+			giveGateways = document.getElementsByClassName('give-gateway');
 		}
 
+		for (let c = 0; c < giveGateways.length; c++) {
+			var isChecked = giveGateways[c].getAttribute('checked');
+			if (isChecked) {
+				return giveGateways[c].value;
+			}
+		}
 	}
 
 	function update_fee(amount) {
+		console.log('atualizar taxa de transação ' + amount);
 		var iframe = document.getElementsByName('give-embed-form')[0];
-		var gatewaySelected = get_selected_gateway(iframe, feeGateways);
+		// var gatewaySelected = get_selected_gateway(iframe, feeGateways);
+		// var feeRecovery = getElementById('lkn-fee-recovery-enabled');
+
+		// console.log('gateway selected: ' + gatewaySelected);
 
 		if (iframe) {
+			//var feeGateways = JSON.parse(iframe.contentDocument.getElementById('lkn-fee-recovery-fee-gateway').value);
+
+			//console.log('recovery fee gateway: ' + JSON.stringify(feeGateways[gatewaySelected].fee_fixed));
+
 			var checkboxLabel = iframe.contentDocument.getElementsByClassName('lkn-fee-recovery-label')[0];
+			var originalLabel = iframe.contentDocument.getElementById('lkn-fee-recovery-original-description').value;
 
 			var feeValue = parseFloat(iframe.contentDocument.getElementById('lkn-fee-recovery-value').value);
 			var feePercent = parseFloat(iframe.contentDocument.getElementById('lkn-fee-recovery-percent').value);
 			var feeTotal = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format((amount * feePercent) + feeValue);
 
-			checkboxLabel.innerHTML = checkboxLabel.innerHTML.replaceAll('##', feeTotal);
+			checkboxLabel.innerHTML = originalLabel.replaceAll('##', feeTotal);
 		} else {
 			// TODO on gateway select recalculate fees
 			//var feeGateways = JSON.parse($('#lkn-fee-recovery-fee-gateway').val());
-			//console.log('recovery fee gateway: ' + JSON.stringify(feeGateways.manual.fee_fixed));
+			//console.log('recovery fee gateway: ' + JSON.stringify(feeGateways[gatewaySelected].fee_fixed));
 
 			var checkboxLabel = $('.lkn-fee-recovery-label');
+			var originalLabel = $('#lkn-fee-recovery-original-description').val();
 
 			var feeValue = parseFloat($('#lkn-fee-recovery-value').val());
 			var feePercent = parseFloat($('#lkn-fee-recovery-percent').val());
 			var feeTotal = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format((amount * feePercent) + feeValue);
 
-			checkboxLabel.html(checkboxLabel.html().replaceAll('##', feeTotal));
+			checkboxLabel.html(originalLabel.replaceAll('##', feeTotal));
 		}
 	}
 })(jQuery);
