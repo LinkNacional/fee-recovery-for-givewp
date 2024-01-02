91a0f3aae1891e967ac556ee79498ec5eef48e04
Emanuel Lopes <lopes.emanuel@linknacional.com>
2024-01-02 19:23:17 +0000

    fix: Avoid direct access to PHP files #9

--- fee-recovery-for-givewp.php
+++ fee-recovery-for-givewp.php
@@ -25,7 +25,7 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
--- public/partials/fee-recovery-for-givewp-public-display.php
+++ public/partials/fee-recovery-for-givewp-public-display.php
@@ -1,5 +1,10 @@
 <?php
 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
 /**
  * Provide a public-facing view for the plugin.
  *
@@ -11,19 +16,24 @@
 ?>
 
 <div id="lkn-fee-recovery-checkbox-wrapper">
-    <input type="hidden" id="lkn-fee-recovery-value" value="<?php esc_attr_e($args['feeValue']); ?>" />
-    <input type="hidden" id="lkn-fee-recovery-percent" value="<?php esc_attr_e($args['feeValuePercent']); ?>" />
-    <input type="hidden" id="lkn-fee-recovery-enabled" value="<?php esc_attr_e($args['feeEnabled']); ?>" />
-    <input type="hidden" id="lkn-fee-recovery-original-description" value="<?php esc_attr_e($args['description']); ?>" />
+	<input type="hidden" id="lkn-fee-recovery-value"
+		value="<?php esc_attr_e($args['feeValue']); ?>" />
+	<input type="hidden" id="lkn-fee-recovery-percent"
+		value="<?php esc_attr_e($args['feeValuePercent']); ?>" />
+	<input type="hidden" id="lkn-fee-recovery-enabled"
+		value="<?php esc_attr_e($args['feeEnabled']); ?>" />
+	<input type="hidden" id="lkn-fee-recovery-original-description"
+		value="<?php esc_attr_e($args['description']); ?>" />
 
-    <ul id="lkn-fee-recovery-list-wrap">
-        <li id="lkn-fee-recovery-list" style="list-style-type: none;padding: 0px 0px !important;background-color: #ffffff;border: none;">
-            <div id="lkn-fee-recovery-wrap">
-                <input class="give-input" type="checkbox" id="lkn-fee-recovery-input" name="lkn-fee-recovery"
-                    value="no">
-                <label class="lkn-fee-recovery-label"
-                    for="lkn-fee-recovery-input"><?php esc_html_e($args['description'], FEE_RECOVERY_FOR_GIVEWP_TEXT_DOMAIN); ?></label>
-            </div>
-        </li>
-    </ul>
+	<ul id="lkn-fee-recovery-list-wrap">
+		<li id="lkn-fee-recovery-list"
+			style="list-style-type: none;padding: 0px 0px !important;background-color: #ffffff;border: none;">
+			<div id="lkn-fee-recovery-wrap">
+				<input class="give-input" type="checkbox" id="lkn-fee-recovery-input" name="lkn-fee-recovery"
+					value="no">
+				<label class="lkn-fee-recovery-label"
+					for="lkn-fee-recovery-input"><?php esc_html_e($args['description'], FEE_RECOVERY_FOR_GIVEWP_TEXT_DOMAIN); ?></label>
+			</div>
+		</li>
+	</ul>
 </div>
